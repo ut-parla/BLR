2fbb5339ea1a6ce7ecbd1481ca96b87db333e6df
Henrique Fingler <hfingler@c196-012.frontera.tacc.utexas.edu>
2022-03-24 21:18:59 -0500

    fix

--- app/auto.py
+++ app/auto.py
@@ -1,9 +1,13 @@
 from subprocess import run
 from time import sleep
 
-parts = ["2000", "5000"]
+#parts = ["2000", "5000"]
+parts = ["50"] 
 rtypes = ["cpu_blr", "mgpu_blr", "parla"]
-sizes = ["10k", "20k"]
+#sizes = ["10k", "20k"]
+sizes = ["20k"]
+sizes = ["1k"]
+
 
 template_cmd = "python app/main.py run {rtype} inputs/Arand{size}.mat.npy inputs/xrand{size}.mat.npy {psize} > {fout}"
 
@@ -12,5 +16,6 @@ for p in parts:
         for isize in sizes:
             fname = f"{rt}_part{p}_inp{isize}.txt"
             cmd = template_cmd.format(rtype=rt, size=isize, psize=p, fout=fname)
+            print("running  ", cmd)             
             run(cmd, shell=True)
             sleep(2)
